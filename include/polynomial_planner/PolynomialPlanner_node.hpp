de710b4d109d2bffa4c52ad7a5430bf1dc930ce1
redto0 <=>
2025-02-13 17:36:06 -0500

    added includes

--- include/polynomial_planner/PolynomialPlanner_node.hpp
+++ include/polynomial_planner/PolynomialPlanner_node.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
-#include <nav_msgs/msg/path.hpp>
+// #include <nav_msgs/msg/path.hpp>
+
+#include "opencv2/core/types.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+// #include "std_msgs/msg/string.hpp"
 
 class PolynomialPlanner : public rclcpp::Node {
 private:
@@ -15,11 +21,12 @@ private:
     std::unique_ptr<std::optional<nav_msgs::msg::Path>> Backend;
 
     // TF2 stuff
-    // std::unique_ptr<tf2_ros::TransformListener> tf2_listener;
-    // std::unique_ptr<tf2_ros::Buffer> tf2_buffer;
+    td::unique_ptr<tf2_ros::TransformListener> tf2_listener;
+    td::unique_ptr<tf2_ros::Buffer> tf2_buffer;
 public:
     PolynomialPlanner(const rclcpp::NodeOptions& options);
 
     /// subscriber callback
     void sub_cb(std_msgs::msg::String::SharedPtr msg);
 };
+
