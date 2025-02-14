0330eea6270a7c9d042c2988506b18b11bbb5d83
redto0 <=>
2025-02-13 20:00:43 -0500

    reformat for build

--- include/polynomial_planner/PolynomialPlanner_node.hpp
+++ include/polynomial_planner/PolynomialPlanner_node.hpp
@@ -4,11 +4,8 @@
 
 #include "opencv2/core/types.hpp"
 #include "rclcpp/rclcpp.hpp"
-
 #include "tf2_ros/buffer.h"
 #include "tf2_ros/transform_listener.h"
-
-#include "rclcpp/rclcpp.hpp"
 // #include "std_msgs/msg/string.hpp"
 
 class PolynomialPlanner : public rclcpp::Node {
@@ -23,10 +20,10 @@ private:
     // TF2 stuff
     td::unique_ptr<tf2_ros::TransformListener> tf2_listener;
     td::unique_ptr<tf2_ros::Buffer> tf2_buffer;
+
 public:
     PolynomialPlanner(const rclcpp::NodeOptions& options);
 
     /// subscriber callback
     void sub_cb(std_msgs::msg::String::SharedPtr msg);
 };
-
