130bd2132ac4960fd8888b1359edeaca298268a3
Alexander Boccaccio <aboccacc@umich.edu>
2025-02-14 17:49:17 -0500

    switch from two polynomial to one

--- src/backend.cpp
+++ src/backend.cpp
@@ -4,8 +4,7 @@
 
 #include "geometry_msgs/msgs/PoseStamped.hpp"
 
-std::optional<nav_msgs::msg::Path> backend::create_path(const std::vector& leftPoly,
-                                                        const std::vector& rightPoly std::string_view frame) {
+std::optional<nav_msgs::msg::Path> backend::create_path(const std::vector& leftPoly, std::string_view frame) {
     // std::string_view is a string lol
     std::vector<cv::Point2d> path;  // this is the array for cone points
                                     // this will make the
@@ -19,72 +18,32 @@ std::optional<nav_msgs::msg::Path> backend::create_path(const std::vector& leftP
 
     for (int i = 0; i < /* listenerArry.length*/; i++) {
         is_left_valid = (leftPoly == NULL) ? is_left_valid : (leftPoly[i] != 0) ? true : is_left_valid;
-
-        is_right_valid = (rightPoly == NULL) ? is_right_valid : (rightPoly[i] != 0) ? true : is_right_valid;
     }
     leftPoly = (is_left_valid) ? new Polynomial(/* vector from ros listener */) : null;
     if (is_left_valid) {
-        Polynomistd_msgs::Stringal leftPoly = new Polynomial(/* vector from ros listener */);
+        Polynomial leftPoly = new Polynomial(/* vector from ros listener */);
     } else {
-        std_msgs::String
             // TODO this is lazy and bad fix please
             Polynomial leftPoly = null;
     }
-    rightPoly = (is_right_valid) ? new Polynomial(/* vector from ros listener */) : null;
-    if (is_right_valid) {
-        Polynomial rightPoly = new Polynomial(/* vector from ros listener */);
-    } else {
-        // TODO this is lazy and bad fix please
-        Polynomial rightPoly = null;
-    }
 
     // interval for polynomial
-    float max = 10;
-    float interval = 0.25;
-    float start = 0;
-    std::vector<cv::Point2d> nav_points;
-    for (float i = start; i < max; i += interval) {
-        // generate points
-        nav_points.pushback();
-        if (leftPoly != null) {
-            // do left poly math;
-            // <y, -x>
-            float dx = leftPoly.polyDirvative(x);
-            // calulate dx were dy is 1
-            // becuase I dont want to use a wrapper class
-            float dy = 1;
-            // set dy to 1
-            float l = std::sqrt(dx * dx + dy * dy);
-            // find the magnitude
-            dx = dx / l;  // normalize
-            dy = dy / l;  // normalize
-            cv::Point2d temp;
-            float x = i;                 // define x
-            float y = leftPoly.poly(x);  // define y
-            temp.x = (x + 7 * dy);       // project x
-            temp.y = (y - 7 * dx);       // project y
-            nav_points[i].pushback(temp);
-            // return vector as < y, -x >
+    float max = 200;
+    float interval = 3;
+    float start = 5;
+    float threshold = 15.0;
+    
+    // TODO this is lazy and bad fix please
+    float dist = 0;
+    for (int i = start; i < max; i += interval) {
+
+        dist += sqrt(interval * interval + pow(leftPoly.poly(i) - leftPoly.poly(i + interval), 2));
+
+        if (dist > threshold) {
+            path.pushback(cv::Point2d(i,leftPoly.poly(i)));
+            dist = 0;
         }
-        if (rightPoly != null) {
-            // do right poly math
-            // <-y, x>
-            // same steps different numbers
-            float dx = rightPoly.polyDirvative(x);
-            float dy = 1;
-            float l = std::sqrt(dx * dx + dy * dy);
-            l = std::hypot(dx, dy);
-            dx = dx / l;
-            dy = dy / l;
-            cv::Point2d temp;
-            float x = i;
-            float y = leftPoly.poly(x);
-            temp.x = (x - 7 * dy);
-            temp.y = (y + 7 * dx);
-            nav_points[i].pushback(temp);
-            // return vector as < -y , x >
-        }
-        // rememebr to return
+
     }
 
     if (path.empty()) {
