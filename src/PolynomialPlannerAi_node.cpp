e852525ec4ca2ba0c9c72cd62f3a3bcbbdeb5f73
redto0 <aboccacc@umich.edu>
2025-02-20 14:50:48 -0500

    make derek happy with code formats

--- src/PolynomialPlannerAi_node.cpp
+++ src/PolynomialPlannerAi_node.cpp
@@ -1,25 +1,22 @@
 #include "polynomial_planner/PolynomialPlannerAi_node.hpp"
 
-
 // Required for doTransform
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-
-
-#include <vector>
 #include <cmath>
+#include <vector>
+
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 using namespace std::placeholders;
 
 PolynomialPlannerAi::PolynomialPlannerAi(const rclcpp::NodeOptions& options) : Node("polynomial_planner_ai", options) {
     this->poly_sub = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "/road/polynomial", 1,
-            std::bind(&PolynomialPlannerAi::polynomial_cb, this, _1));
+        "/road/polynomial", 1, std::bind(&PolynomialPlannerAi::polynomial_cb, this, _1));
 
     this->path_pub = this->create_publisher<nav_msgs::msg::Path>("/path", 5);
 
     this->rgb_info_sub = this->create_subscription<sensor_msgs::msg::CameraInfo>(
-            "/camera/mid/rgb/camera_info", 1,
-            [this](sensor_msgs::msg::CameraInfo::ConstSharedPtr ci) { this->rgb_model.fromCameraInfo(ci); });
+        "/camera/mid/rgb/camera_info", 1,
+        [this](sensor_msgs::msg::CameraInfo::ConstSharedPtr ci) { this->rgb_model.fromCameraInfo(ci); });
 
     RCLCPP_INFO(this->get_logger(), "PolynomialPlannerAi Node Started! Waiting for polynomial data...");
 }
@@ -36,7 +33,6 @@ void PolynomialPlannerAi::polynomial_cb(std_msgs::msg::Float32MultiArray::Shared
         RCLCPP_INFO(this->get_logger(), "Coefficient[%zu] = %.15e", i, msg->data[i]);
     }
 }
-
 
 void PolynomialPlannerAi::evaluate_polynomial(const std::vector<float>& coeffs, const std::vector<float>& x_values) {
     for (float x : x_values) {
