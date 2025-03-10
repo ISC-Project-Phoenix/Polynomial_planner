9fdc4182b526ad18c602e3dd1e4614fa4aa5470f
Alexander Boccaccio <aboccacc@umich.edu>
2025-03-09 21:40:47 -0400

    Poly class fix backend.hpp
    Signed-off-by: Alexander Boccaccio <aboccacc@umich.edu>

--- include/polynomial_planner/backend.hpp
+++ include/polynomial_planner/backend.hpp
@@ -23,7 +23,7 @@ public:
     float poly(float x) {
         float result = 0;
         for (int i = 0; i < vect.size(); i++) {
-            int power = vect.size() - i;
+            int power = vect.size() - i - 1;
             // TODO this pow function might be wrong
             // inproper way of accessing vect index
             result += vect[i] * pow(x, power);  // a[n] * x ^ n
@@ -33,9 +33,9 @@ public:
     float polyDirvative(float x) {
         float result = 0;
         for (int i = 0; i < vect.size() - 1; i++) {
-            int power = vect.size() - i - 1;
+            int power = vect.size() - i - 2;
             // todo finish?
-            result += vect[i] * i * pow(x, power);  // a[n] * n * x ^ (n - 1)
+            result += vect[i] * (power + 1) * pow(x, power);  // a[n] * n * x ^ (n - 1)
         }
     }
 };
